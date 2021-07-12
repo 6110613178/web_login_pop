6b2b5a89728d2611c00e97bf31b54dda59ce8175
unknown <torres-jirat@hotmail.com>
2021-07-12 11:23:00 +0700

    FrontEnd_RegisterLogin

--- billion/resources/views/auth/login.blade.php
+++ billion/resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
+<!-- <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -82,4 +82,170 @@
     });
     </script>
 @endif
+@endsection -->
+
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header" >
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+  <div class="container-fluid">
+    <div class="row">
+        
+          <div class="col-8 col-sm-6">
+          <form method="POST" action="{{ route('login') }}">
+
+                    <h5  align = 'center' class="modal-title" id="loginModal" >{{ __('เข้าสู่ระบบเพื่อดำเนินการต่อ') }}</h5>
+                    <br>
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="email" class="col-form-label">{{ __('ชื่อผู้ใช้/อีเมล') }}</label>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus style="background-color:#D1FBD2">
+                        @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="col-form-label">{{ __('รหัสผ่าน') }}</label>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" style="background-color:#D1FBD2">
+                        @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+
+                    <div class="row justify-content">
+                        <div class="col-7">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('จดจำข้อมูลเข้าสู่ระบบไว้') }}
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}" text-md-right>
+                                    {{ __('ลืมรหัสผ่าน?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+
+                  
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-2">
+                            <button type="submit" class="btn  btn-block " style="background-color:#9AF099">
+                                {{ __('เข้าสู่ระบบ') }}
+                            </button>
+                        </div>
+                    </div>
+                
+                </form>
+                  <br>
+                <div class="rows">
+                  <div class="column"> <hr class="vertical"> </div>
+                  <div class="column" align = 'center'> {{ __('หรือ') }} </div>
+                  <div class="column"> <hr class="vertical"> </div>
+                </div>
+
+                 
+              
+          </div>
+
+          <div class="col-8 col-sm-1">
+          <img src="<?php echo asset('img/2.png'); ?>" style="width:110%">
+</div>
+        
+          <div class="col-8 col-sm-5">
+        
+          
+<div class="slideshow-container">
+
+<div class="mySlides faded">
+  <div class="numbertext">1 / 3</div>
+  <img src="<?php echo asset('img/624269.jpg'); ?>" style="width:100%">
+</div>
+
+
+<div class="mySlides faded">
+  <div class="numbertext">2 / 3</div>
+  <img src="<?php echo asset('img/19972.jpg'); ?>" style="width:100%">
+</div>
+
+<div class="mySlides faded">
+  <div class="numbertext">3 / 3</div>
+  <img src="<?php echo asset('img/19975.jpg'); ?>" style="width:100%">
+</div>
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
+</div>
+
+@section('scripts')
+@parent
+
+@if($errors->has('email') || $errors->has('password'))
+    <script>
+    $(function() {
+        $('#loginModal').modal({
+            show: true
+        });
+    });
+    </script>
+@endif
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
 @endsection
--- billion/resources/views/auth/register.blade.php
+++ billion/resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
+<!-- <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -101,4 +101,149 @@ $(function () {
     });
 })
 </script>
+@endsection -->
+
+
+<div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-contentt">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <h5 align = 'center' class="modal-title" id="registerModal">{{ __('สมัครสมาชิก') }}</h5>
+            <div class="modal-body">
+                <form method="POST" id="registerForm">
+            
+                    @csrf
+
+                        <div class="mb-2">
+                        <label for="nameInput" class="col-md-4 col-form-label text-md-left">{{ __('ชื่อ') }}</label>
+                            <input id="nameInput" type="text" class="form-control1" name="name" value="{{ old('name') }}"  autocomplete="name" autofocus style="background-color:#D1FBD2">
+                            <span class="invalid-feedback" role="alert" id="nameError">
+                                <strong></strong>
+                            </span>
+                        </div>
+
+                        <div class="mb-2">
+                        <label for="emailInput" class="col-md-4 col-form-label text-md-left">{{ __('อีเมล') }}</label>
+                        <input id="emailInput" type="email" class="form-control1" name="email" value="{{ old('email') }}" required autocomplete="email" style="background-color:#D1FBD2">
+                            <span class="invalid-feedback" role="alert" id="emailError">
+                                <strong></strong>
+                            </span>
+                        </div>
+
+                        <div class="mb-2">
+                        <label for="telephoneInput" class="col-md-4 col-form-label text-md-left">{{ __('เบอร์โทรศัพท์') }}</label>
+                        <input id="telephoneInput" type="telephone" class="form-control1" name="telephone" value="{{ old('telephone') }}" required autocomplete="telephone" style="background-color:#D1FBD2">
+                        </div>
+
+                        <div class="mb-2">
+                            <label for="passwordInput" class="col-md-4 col-form-label text-md-left">{{ __('รหัสผ่าน') }}</label>
+                            <input id="passwordInput" type="password" class="form-control1" name="password" required autocomplete="new-password" style="background-color:#D1FBD2">
+                            <span class="invalid-feedback" role="alert" id="passwordError">
+                                <strong></strong>
+                            </span>
+                        </div>
+
+                        <div class="mb-2">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-left">{{ __('ยืนยันรหัสผ่าน') }}</label>
+                            <input id="password-confirm" type="password" class="form-control1" name="password_confirmation" required autocomplete="new-password" style="background-color:#D1FBD2"> 
+                        </div>
+
+                        <div class="rowcheck justify-content">
+                        <div class="col-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="status" id="status" {{ old('status') ? 'buyer' : '' }}>
+
+                                <label class="form-check-label" for="status">
+                                    {{ __('ผู้ซื้อ') }}
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="status" id="status" {{ old('status') ? 'owner' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('เจ้าของทรัพย์') }}
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                        <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="status" id="status" {{ old('remember') ? 'agent' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('เอเจนท์') }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content">
+                        <div class="col-7">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('ยอมรับเงื่อนไขข้อตกลง') }}
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-4.">
+                           <label class="form-check-label1" data-toggle="collapse" data-target="#demo" aria-expanded="false">รายละเอียด</label>
+                           <div id="demo" class="collapse"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>    
+                        </div>
+                    </div>
+                  
+                    <br>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-block" style="background-color:#9AF099">
+                                {{ __('สมัครสมาชิก') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@section('scripts')
+@parent
+
+<script>
+$(function () {
+    $('#registerForm').submit(function (e) {
+        e.preventDefault();
+        let formData = $(this).serializeArray();
+        $(".invalid-feedback").children("strong").text("");
+        $("#registerForm input").removeClass("is-invalid");
+        $.ajax({
+            method: "POST",
+            headers: {
+                Accept: "application/json"
+            },
+            url: "{{ route('register') }}",
+            data: formData,
+            success: () => window.location.assign("{{ route('home') }}"),
+            error: (response) => {
+                if(response.status === 422) {
+                    let errors = response.responseJSON.errors;
+                    Object.keys(errors).forEach(function (key) {
+                        $("#" + key + "Input").addClass("is-invalid");
+                        $("#" + key + "Error").children("strong").text(errors[key][0]);
+                    });
+                } else {
+                    window.location.reload();
+                }
+            }
+        })
+    });
+})
+</script>
 @endsection
+
